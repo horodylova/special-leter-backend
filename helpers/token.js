bba3bfe8b83c39d4f346f1452999f95e51070324
Svitlana Horodylova <horodylova.sv@gmail.com>
2025-01-14 16:06:14 +0000

    preparing to hosting

--- helpers/token.js
+++ helpers/token.js
@@ -1,3 +1,5 @@
+//this code I've used for development. to run, use command: node token
+
 const jwt = require("jsonwebtoken");
 
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNzM2ODU5ODE2LCJleHAiOjE3MzY5NDYyMTZ9.1QBchQp7LUYdz3FJgwHpA86MTAxn8CxviLT6BsEEmRM";  
