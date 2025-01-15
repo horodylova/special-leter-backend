85bdc33bb7a5c4d0305d070af3c52bcabea8ecba
Svitlana Horodylova <horodylova.sv@gmail.com>
2025-01-15 16:43:23 +0000

    Fix database connection: Force IPv4

--- db/connection.js
+++ db/connection.js
@@ -1,43 +1,43 @@
-// const pg = require('pg');
-// const dotenv = require('dotenv');
-// const path = require('path');
-// const { Pool } = pg;
+const pg = require('pg');
+const dotenv = require('dotenv');
+const path = require('path');
+const { Pool } = pg;
 
-// const ENV = process.env.NODE_ENV || 'development';
+const ENV = process.env.NODE_ENV || 'development';
 
-// dotenv.config({
-//   path: path.resolve(__dirname, `../.env.${ENV}`),
-// });
+dotenv.config({
+  path: path.resolve(__dirname, `../.env.${ENV}`),
+});
 
-// if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-//   throw new Error('PGDATABASE or DATABASE_URL not set');
-// }
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE or DATABASE_URL not set');
+}
 
-// const config = {};
+const config = {};
 
-// if (ENV === 'production') {
-//   config.connectionString = process.env.DATABASE_URL;
-//   config.max = 2; 
-//   config.ssl = {
-//     rejectUnauthorized: false, 
-//   };
-// }
-
-// const pool = new Pool(config);
-// module.exports = pool;
-
-const { Pool } = require("pg");
-
-const config = {
-  connectionString: "postgresql://postgres:svetlana123456SVETLANA123456@db.zdyhayhguzfcjuilwnua.supabase.co:5432/postgres",
-  ssl: {
-    rejectUnauthorized: false,  
-  },
-  family: 4,
-};
+if (ENV === 'production') {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2; 
+  config.ssl = {
+    rejectUnauthorized: false, 
+  };
+}
 
 const pool = new Pool(config);
-
 module.exports = pool;
+
+// const { Pool } = require("pg");
+
+// const config = {
+//   connectionString: "postgresql://postgres:svetlana123456SVETLANA123456@db.zdyhayhguzfcjuilwnua.supabase.co:5432/postgres",
+//   ssl: {
+//     rejectUnauthorized: false,  
+//   },
+//   family: 4,
+// };
+
+// const pool = new Pool(config);
+
+// module.exports = pool;
 
 
